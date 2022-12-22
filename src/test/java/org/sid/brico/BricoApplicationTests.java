b4110a6bcecfdc12575583602000799f6b2bca96
saouid mohamed amine <saouid_mohamedamine@emsi-edu.ma>
2022-12-22 23:37:07 +0100

    first commit

---
+++ src/test/java/org/sid/brico/BricoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.brico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BricoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
